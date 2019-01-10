5ad175dae851b1d15fc6fe803f2fedf11b95d464
thisisbrix901 <bmadeira7@gmail.com>
2019-01-09 19:21:12 -0500

    nearly finished

--- build/precache-manifest.74e5f100828c8bb91e196ebfbab946a0.js
+++ build/precache-manifest.74e5f100828c8bb91e196ebfbab946a0.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "a5d2a144b8297e3a2d86",
-    "url": "/portfolio/static/js/main.a5d2a144.chunk.js"
+    "revision": "f55412e95dc4451025d0",
+    "url": "/portfolio/static/js/main.f55412e9.chunk.js"
   },
   {
     "revision": "5c9e5e5e0d28bd38ae0a",
     "url": "/portfolio/static/js/1.5c9e5e5e.chunk.js"
   },
   {
-    "revision": "a5d2a144b8297e3a2d86",
+    "revision": "f55412e95dc4451025d0",
     "url": "/portfolio/static/css/main.e19a8b8f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/1.57059012.chunk.css"
   },
   {
-    "revision": "aed701456713ed8388ec6bb71e2b0caa",
+    "revision": "49737934656d17ff45b9f5eed0cf9167",
     "url": "/portfolio/index.html"
   }
 ];
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -35,7 +35,7 @@ class Projects extends Component {
               Fala Mobile Messaging App
             </CardTitle>
             <CardText>
-              A responsive messaging service built with JSX in React and using Bootstrap and Pusher Chatkit
+              A responsive messaging service built with React, Bootstrap, and Pusher Chatkit
             </CardText>
             <CardActions border>
               <Button colored><a href="https://github.com/bmadeira7/falaChat" rel="noopener noreferrer" target="_blank">Github</a></Button>
@@ -166,18 +166,17 @@ class Projects extends Component {
               color: "black",
               height: "176px",
               background:
-                "url(https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/71627141/original/0a3e1bc516bc4f9d3ffa944010d7ad327cb5303a/fix-jquery-ajax-issues.jpg) center / cover"
+                "url(https://lh3.googleusercontent.com/4yZAa4f5Oe1aj5ga_Xh6A3VRzVGetG_o-MDXhqwoerBWqqDEKfWhxyAUL6YcrTq5Xoe01s2bLMyETztGgtm7n2Cxj3Um0yFm_vixj7dNl-NMSeQulKe1aWX9xxDgPUwBRNoMvOClhhwkaHglPbGLh7gDnLcajwCqwg9mRbtlbfxabR4WxwQiO1NuYnP1HwyRSkZvOs2YQMKPpBTLPC7LOpt708FJKYvLHOoUN4A_U8BPt76Fe2vzHRGsdR5_vfnXw8vIOj0Q1mmbFrcSYSuHfQHOCnyKmZCfDeOU-WtzzFpXbZeRAwnTrGs21SSPV0vE6KfaeTIqQ594mxR_Pk_38qQ2Za-AUn0IQsVQXp_GD90p2pugx98n1rbi6BUSaaC7AV7sUikmmJrRimVj2-lAggoeAQkR3Wq_BzQhvLCbciwxZmVMGjd-fcow-NBKXMAWw3YwIKwSssZv_jNnyf17u1P1zVUzVa-jGvsV7Vfl0iO5k1W9qWXAYV9eUNPSxleBgUaoHogN8gNOMiAoGwYR1JqLBoisxmulnZFtGDPz92op8RZg9I77WUYufyTWv6bjNH8FhEfqAf9zPHgWXRAki--fuRo_phpV7ppwjPBA4Tl1MSzcK65AHfxzuyXuNPTxMBf0wNTqywIifiNr8zj2h-1Y=w372-h235-no"
             }}
           >
-            Ajax project project #1
+            Resort Me
           </CardTitle>
           <CardText>
-            Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
+            An application that assists the user in locating ski resorts based on input
           </CardText>
           <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Heroku</Button>
-            <Button colored>Live-Demo</Button>
+          <Button colored><a href="https://github.com/bmadeira7/Project-2" rel="noopener noreferrer" target="_blank">Github</a></Button>
+              <Button colored style={{float: 'right'}}><a href="http://bloodcurdling-vampire-87131.herokuapp.com/" rel="noopener noreferrer" target="_blank">Live Demo</a></Button>
           </CardActions>
           <CardMenu style={{ color: "fff" }}>
             
@@ -191,129 +190,105 @@ class Projects extends Component {
               color: "black",
               height: "176px",
               background:
-                "url(https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/71627141/original/0a3e1bc516bc4f9d3ffa944010d7ad327cb5303a/fix-jquery-ajax-issues.jpg) center / cover"
+                "url(https://lh3.googleusercontent.com/4yZAa4f5Oe1aj5ga_Xh6A3VRzVGetG_o-MDXhqwoerBWqqDEKfWhxyAUL6YcrTq5Xoe01s2bLMyETztGgtm7n2Cxj3Um0yFm_vixj7dNl-NMSeQulKe1aWX9xxDgPUwBRNoMvOClhhwkaHglPbGLh7gDnLcajwCqwg9mRbtlbfxabR4WxwQiO1NuYnP1HwyRSkZvOs2YQMKPpBTLPC7LOpt708FJKYvLHOoUN4A_U8BPt76Fe2vzHRGsdR5_vfnXw8vIOj0Q1mmbFrcSYSuHfQHOCnyKmZCfDeOU-WtzzFpXbZeRAwnTrGs21SSPV0vE6KfaeTIqQ594mxR_Pk_38qQ2Za-AUn0IQsVQXp_GD90p2pugx98n1rbi6BUSaaC7AV7sUikmmJrRimVj2-lAggoeAQkR3Wq_BzQhvLCbciwxZmVMGjd-fcow-NBKXMAWw3YwIKwSssZv_jNnyf17u1P1zVUzVa-jGvsV7Vfl0iO5k1W9qWXAYV9eUNPSxleBgUaoHogN8gNOMiAoGwYR1JqLBoisxmulnZFtGDPz92op8RZg9I77WUYufyTWv6bjNH8FhEfqAf9zPHgWXRAki--fuRo_phpV7ppwjPBA4Tl1MSzcK65AHfxzuyXuNPTxMBf0wNTqywIifiNr8zj2h-1Y=w372-h235-no"
             }}
           >
-            Ajax project project #2
+           Face The Music
           </CardTitle>
           <CardText>
-            Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
+         Application that recognizes faces and emotion and recommends music from YouTube based on periceved emotion
           </CardText>
           <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Heroku</Button>
-            <Button colored>Live-Demo</Button>
+          <Button colored><a href="https://github.com/bmadeira7/Project-1" rel="noopener noreferrer" target="_blank">Github</a></Button>
+              <Button colored style={{float: 'right'}}><a href="https://bmadeira7.github.io/Project-1/" rel="noopener noreferrer" target="_blank">Live Demo</a></Button>
           </CardActions>
           <CardMenu style={{ color: "fff" }}>
             
           </CardMenu>
         </Card>
 
-        {/* Project 3 */}
-        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-          <CardTitle
-            style={{
-              color: "black",
-              height: "176px",
-              background:
-                "url(https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/71627141/original/0a3e1bc516bc4f9d3ffa944010d7ad327cb5303a/fix-jquery-ajax-issues.jpg) center / cover"
-            }}
-          >
-           Ajax project #3
-          </CardTitle>
-          <CardText>
-            Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Heroku</Button>
-            <Button colored>Live-Demo</Button>
-          </CardActions>
-          <CardMenu style={{ color: "fff" }}>
-            
-          </CardMenu>
-        </Card>
+        
       </div>
       );
-    } else if (this.state.activeTab === 3) {
-      return (
-        <div className="projects-grid">
-        {/* PROJECT 1 */}
-        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-          <CardTitle
-            style={{
-              color: "black",
-              height: "176px",
-              background:
-                "url(https://servoy.com/wp-content/uploads/icon-bootstrap.png) center / cover"
-            }}
-          >
-            Bootstrap project #1
-          </CardTitle>
-          <CardText>
-            Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Heroku</Button>
-            <Button colored>Live-Demo</Button>
-          </CardActions>
-          <CardMenu style={{ color: "fff" }}>
+    // } else if (this.state.activeTab === 3) {
+    //   return (
+    //     <div className="projects-grid">
+    //     {/* PROJECT 1 */}
+    //     <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+    //       <CardTitle
+    //         style={{
+    //           color: "black",
+    //           height: "176px",
+    //           background:
+    //             "url(https://servoy.com/wp-content/uploads/icon-bootstrap.png) center / cover"
+    //         }}
+    //       >
+    //         Bootstrap project #1
+    //       </CardTitle>
+    //       <CardText>
+    //         Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
+    //       </CardText>
+    //       <CardActions border>
+    //         <Button colored>GitHub</Button>
+    //         <Button colored>Heroku</Button>
+    //         <Button colored>Live-Demo</Button>
+    //       </CardActions>
+    //       <CardMenu style={{ color: "fff" }}>
             
-          </CardMenu>
-        </Card>
+    //       </CardMenu>
+    //     </Card>
 
-        {/* PROJECT 2 */}
-        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-          <CardTitle
-            style={{
-              color: "black",
-              height: "176px",
-              background:
-                "url(https://servoy.com/wp-content/uploads/icon-bootstrap.png) center / cover"
-            }}
-          >
-           Bootstrap project #2
-          </CardTitle>
-          <CardText>
-            Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Heroku</Button>
-            <Button colored>Live-Demo</Button>
-          </CardActions>
-          <CardMenu style={{ color: "fff" }}>
+    //     {/* PROJECT 2 */}
+    //     <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+    //       <CardTitle
+    //         style={{
+    //           color: "black",
+    //           height: "176px",
+    //           background:
+    //             "url(https://servoy.com/wp-content/uploads/icon-bootstrap.png) center / cover"
+    //         }}
+    //       >
+    //        Bootstrap project #2
+    //       </CardTitle>
+    //       <CardText>
+    //         Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
+    //       </CardText>
+    //       <CardActions border>
+    //         <Button colored>GitHub</Button>
+    //         <Button colored>Heroku</Button>
+    //         <Button colored>Live-Demo</Button>
+    //       </CardActions>
+    //       <CardMenu style={{ color: "fff" }}>
             
-          </CardMenu>
-        </Card>
+    //       </CardMenu>
+    //     </Card>
 
-        {/* Project 3 */}
-        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
-          <CardTitle
-            style={{
-              color: "black",
-              height: "176px",
-              background:
-                "url(https://servoy.com/wp-content/uploads/icon-bootstrap.png) center / cover"
-            }}
-          >
-            Bootstrap project #3
-          </CardTitle>
-          <CardText>
-            Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>Heroku</Button>
-            <Button colored>Live-Demo</Button>
-          </CardActions>
-          <CardMenu style={{ color: "fff" }}>
+    //     {/* Project 3 */}
+    //     <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+    //       <CardTitle
+    //         style={{
+    //           color: "black",
+    //           height: "176px",
+    //           background:
+    //             "url(https://servoy.com/wp-content/uploads/icon-bootstrap.png) center / cover"
+    //         }}
+    //       >
+    //         Bootstrap project #3
+    //       </CardTitle>
+    //       <CardText>
+    //         Lorem ipums shf sfshf sfs afsd g fgd d dfh dh dfh gf jf jfg jfh j
+    //       </CardText>
+    //       <CardActions border>
+    //         <Button colored>GitHub</Button>
+    //         <Button colored>Heroku</Button>
+    //         <Button colored>Live-Demo</Button>
+    //       </CardActions>
+    //       <CardMenu style={{ color: "fff" }}>
             
-          </CardMenu>
-        </Card>
-      </div>
-      );
+    //       </CardMenu>
+    //     </Card>
+    //   </div>
+    //   );
     }
   }
 
@@ -327,11 +302,11 @@ class Projects extends Component {
         >
           <Tab>React</Tab>
           <Tab>Javascript/jQuery</Tab>
-          <Tab>JS with API's</Tab>
-          <Tab>Bootstrap</Tab>
+          <Tab>Using AJAX</Tab>
+          {/* <Tab>Bootstrap</Tab> */}
         </Tabs>
 
-        <Grid>
+        <Grid className="project-grid-main">
           <Cell col={12}>
             <div className="content">{this.toggleCategories()}</div>
           </Cell>
