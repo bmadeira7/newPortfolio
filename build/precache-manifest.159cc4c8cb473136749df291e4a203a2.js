5ca73eefe00b0501482abccf04fd11d51a0f1fb6
thisisbrix901 <bmadeira7@gmail.com>
2019-01-10 23:48:45 -0500

    pic fix

--- build/precache-manifest.159cc4c8cb473136749df291e4a203a2.js
+++ build/precache-manifest.159cc4c8cb473136749df291e4a203a2.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "f55412e95dc4451025d0",
-    "url": "/portfolio/static/js/main.f55412e9.chunk.js"
+    "revision": "6770b2f09d2ca087c163",
+    "url": "/portfolio/static/js/main.6770b2f0.chunk.js"
   },
   {
     "revision": "5c9e5e5e0d28bd38ae0a",
     "url": "/portfolio/static/js/1.5c9e5e5e.chunk.js"
   },
   {
-    "revision": "f55412e95dc4451025d0",
-    "url": "/portfolio/static/css/main.e19a8b8f.chunk.css"
+    "revision": "6770b2f09d2ca087c163",
+    "url": "/portfolio/static/css/main.e7873ea8.chunk.css"
   },
   {
     "revision": "5c9e5e5e0d28bd38ae0a",
     "url": "/portfolio/static/css/1.57059012.chunk.css"
   },
   {
-    "revision": "49737934656d17ff45b9f5eed0cf9167",
+    "revision": "3c483e891ed3f16965b57bb2d93a13f0",
     "url": "/portfolio/index.html"
   }
 ];
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ class Landing extends Component {
       <div style={{ width: "100%", margin: "auto" }}>
         <Grid className="landing-grid">
           <Cell col={12}>
-          <img src="https://lh3.googleusercontent.com/y4E74ATWfTit9un6cs1GmaGqLQx5z9AzEGepWk6XuEHxasMDhTRWBm9kW03NRBfdE0x6m3QObzB__ERoK0akwAOJct3kziTmQQlZkehDKhJ71plV0T_Wxz0m2Cfd1y9H5bVJdfDRfQ0QIPHHtzJFVspJK664X5Frmk84G5Hv1dLZfDWEtop_d4NmsuBv3bb8YFpE6Ul_T8W6WwWZs2vct6UnIaEhT__lzwIMtRI2sF5guctnGTYo1wDXuEbZ6B6HwLhpPkAGwMWcmQmGCGsX3Do8_bK94AU07keq71dOVCWwj556eYsEH0KPXOT_wBz7SWjJEd8pX3tl6AMREE9H4uXKOwPvRMtspPIBExQJB0SxSLBQJQJFTsii2v_2GlM2HAodXkJDipk1uYrLfdTCxqUy_a7V3WHbQ1ZfHllW44YjKOkYthdtknxytmCNxFDjn-W3SxrP9esAwW-TZQHiN80vSdJIVBDgMfRJn4vXzwmsuB_F4OyZE2yiZcIPhfEWDplsjBDfb1iAfmD3Is1B2qnoBKJahd0sSDOSpogrEmVGLA1gHABLTWMrWI04mjk6e18dYPU3RdssjeOLjLKgVJkpnML86nS4vGvKT7wALqlR_8g8oW6_85pSuWnIxHegzhoPqsgyNya05lbkrhdsZ6vO=w719-h767-no" alt="avatar" className="avatar-landing" />
+          <img src="/brian-wearing-blk-tee.png" alt="avatar" className="avatar-landing" />
 
             <div className="banner-text">
               <h1>Front End Web Developer</h1>
