509a4b2bdb97b7ee81fae106866a65f50aa2ebf2
thisisbrix901 <bmadeira7@gmail.com>
2019-02-06 20:55:25 -0500

    Updates

--- precache-manifest.0fe9b6446c26b0cddf334ba8cfdabb95.js
+++ precache-manifest.0fe9b6446c26b0cddf334ba8cfdabb95.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "7058c7b7cb4ad565c591",
-    "url": "/portfolio/static/js/main.7058c7b7.chunk.js"
+    "revision": "85a843c8c10d8818a419",
+    "url": "/portfolio/static/js/main.85a843c8.chunk.js"
   },
   {
     "revision": "5c9e5e5e0d28bd38ae0a",
     "url": "/portfolio/static/js/1.5c9e5e5e.chunk.js"
   },
   {
-    "revision": "7058c7b7cb4ad565c591",
+    "revision": "85a843c8c10d8818a419",
     "url": "/portfolio/static/css/main.e7873ea8.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/1.57059012.chunk.css"
   },
   {
-    "revision": "084ce2a4d90a41a14fa6005211b8c987",
+    "revision": "c31e3b40b6bf8c301d347e9d72adb6a3",
     "url": "/portfolio/index.html"
   }
 ];
